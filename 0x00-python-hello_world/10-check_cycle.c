9c5f5dd08eb285523a91e66423adc49f5ec44f58
imane <imanezenasnii@gmail.com>
2023-09-04 20:24:47 +0100

    Add C function check_cycle to determine if a linked list has a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - Checks if a linked list has a cycle
+ * @list: A pointer to the head of the linked list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *current = list;
+
+	while (current)
+	{
+		current = current->next;
+
+		if (current == list)
+			return (1);
+
+	}
+
+	return (0);
+}
