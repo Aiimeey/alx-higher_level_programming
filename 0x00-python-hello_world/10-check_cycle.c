ecfc7dde6ffab04eedfe2d97934910476cd04f71
imane <imanezenasnii@gmail.com>
2023-09-05 01:25:04 +0100

    Add C function check_cycle to determine if a linked list has a cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,19 +8,19 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *current = list;
+	listint_t *ptr1 = list;
+	listint_t *ptr2 = list;
 
-
-	if (list == NULL)
+	if (!list)
 		return (0);
 
-	while (current)
+	while (ptr2 && ptr2->next)
 	{
-		current = current->next;
+		ptr1 = ptr1->next;
+		ptr2 = ptr2->next->next;
 
-		if (current == list)
+		if (ptr1 == ptr2)
 			return (1);
-
 	}
 
 	return (0);
