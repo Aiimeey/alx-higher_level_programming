acdb06a7b07bf6b85b8b7d04271ce462b4d6a274
imane <imanezenasnii@gmail.com>
2023-09-05 14:40:21 +0100

    Add a Python program to extract and print the last digit of a given number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 last_num = number % 10
+if number < 0:
+    last_num *= -1
 if last_num > 5:
     str = "Last digit of {} is {} and is greater than 5"
 elif last_num == 0:
     str = "Last digit of {} is {} and is 0"
-elif last_num < 6 and not 0:
+else:
     str = "Last digit of {} is {} and is less than 6 and not 0"
 print(str.format(number, last_num))
