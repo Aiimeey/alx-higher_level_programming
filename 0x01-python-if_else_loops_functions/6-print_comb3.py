d1bfa1f553b69319f9c29c4652191685fdf1bb33
imane <imanezenasnii@gmail.com>
2023-09-05 17:50:21 +0100

    Add a program that generate and display all possible combinations of two-digit numbers from 00 to 99

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for x in range(0, 9):
     for y in range(0, 10):
         if x < y:
-            if x != 8 and y != 9:
+            if x != 8 or y != 9:
                 print("{}{}".format(x, y), end=", ")
             elif x == 8 and y == 9:
                 print("{}{}".format(x, y))
