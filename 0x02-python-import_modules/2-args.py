3ced2c9d29986b2867bd75e6f617e3cb2af887c7
imane <imanezenasnii@gmail.com>
2023-09-07 20:27:22 +0100

    Task 2: Create a script that handles command line arguments dynamically

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    len_args = len(sys.argv) - 1
+
+    args = sys.argv[1:]
+    if len_args == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len_args))
+
+        for i in range(1, len_args + 1):
+            print("{}: {}".format(i, sys.argv[i]))
