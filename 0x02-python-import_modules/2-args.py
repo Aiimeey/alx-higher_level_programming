fdf9097526c488fad07d880d61b1fee1d0045a1f
imane <imanezenasnii@gmail.com>
2023-09-07 20:36:56 +0100

    Add a script that handles command line arguments dynamically

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,12 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     if len_args == 0:
         print("0 arguments.")
+
+    elif len_args == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+        
+
     else:
         print("{} arguments:".format(len_args))
 
