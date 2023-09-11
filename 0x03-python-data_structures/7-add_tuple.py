12c2e8080124a7341e986ad08b8b9bea7511a12a
imane <imanezenasnii@gmail.com>
2023-09-11 12:13:20 +0100

    Added a Python function to add two tuples element-wise and return a new tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     sum = ()
-
-    tuple_len = max(len(tuple_a), len(tuple_b))
-
-    for i in range(tuple_len):
+    for i in range(2):
         a = tuple_a[i] if i < len(tuple_a) else 0
         b = tuple_b[i] if i < len(tuple_b) else 0
         sum += (a + b,)
