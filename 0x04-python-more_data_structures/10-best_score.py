cbfaffe7bff9befaad311f6dcd07950c679815ab
imane <imanezenasnii@gmail.com>
2023-09-14 03:35:22 +0100

    Create a function that identifies and returns the key associated with the highest integer value in a dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
     if a_dictionary is not None:
         b_score = max(a_dictionary)
         return b_score
