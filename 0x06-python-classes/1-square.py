028da7129ec11a0b8bd9cc22200379312900624e
imane <imanezenasnii@gmail.com>
2023-09-26 20:39:03 +0100

    Added a private instance attribute 'size' and its instantiation

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Class square """
+
+
+class Square:
+    """A class for creating and manipulating square objects """
+
+    def __init__(self, size):
+        """Initialize a new Square instance
+
+        Args:
+            size (int): The length of the square.
+        """
+        self.__size = size
