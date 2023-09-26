eaa3b46dc9ee13aff57790a8868a7f8bb267bc30
imane <imanezenasnii@gmail.com>
2023-09-26 20:54:18 +0100

    Implemented size validation in the Square class raising exceptions for invalid sizes

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+class Square:
+    def __init__(self, size=0):
+        """
+        Initialize a Square object with a given size
+
+        Args:
+            size (int): The size of the square default to 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
