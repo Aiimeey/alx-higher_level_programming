6207649f5f11ae5ebc3611f528c53be31aa666cd
imane <imanezenasnii@gmail.com>
2023-10-07 01:44:46 +0100

    impelment matrix_divided function

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,9 +1,16 @@
 def matrix_divided(matrix, div):
-    if not all(isinstance(row, list) and \
-    all(isinstance(element, (int, float)) for element in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(
+        isinstance(row, list) and
+        all(isinstance(element, (int, float)) for element in row)
+        for row in matrix
+    ):
+        raise TypeError("matrix must be a matrix (list of lists) of \
+        integers/floats")
 
-    if not all(len(row) == len(matrix[0]) for row in matrix):
+    if not all(
+        len(row) == len(matrix[0])
+        for row in matrix
+    ):
         raise TypeError("Each row of the matrix must have the same size")
 
     if not isinstance(div, (int, float)):
@@ -12,6 +19,9 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    result_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+    result_matrix = [
+        [round(element / div, 2) for element in row]
+        for row in matrix
+    ]
 
     return result_matrix
