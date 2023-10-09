89658477c84473e0e8bcd49dd716eaa4200e0abc
imane <imanezenasnii@gmail.com>
2023-10-10 00:13:54 +0100

    Extend BaseGeometry with integer_validator method for attribute validation

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,5 +19,5 @@ class BaseGeometry():
         """
         if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
+        elif value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
