6c233203d2bb4f091bb7b2bce628a0d50cf042f8
imane <imanezenasnii@gmail.com>
2023-10-09 21:39:05 +0100

    Create an empty class BaseGeometry to serve as a base class for geometric shapes

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module provides a base class for geometric shapes and operations
+"""
+
+
+class BaseGeometry():
+    """
+    A base class for geometric shapes and operations
+    """
+    pass
