8f339c7e7ffd50fe7983ef226afd20f6acbbcd33
imane <imanezenasnii@gmail.com>
2023-10-09 19:03:30 +0100

    Create MyList class that inherits from list and adds print_sorted method

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,7 +4,7 @@ class MyList(list):
     def print_sorted(self):
         """
         Print the elements of the list in ascending sorted order
-        
+
         """
         sorted_list = sorted(self)
         print(sorted_list)
