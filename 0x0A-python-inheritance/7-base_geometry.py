9fef9bbb0ee706e88f24d2a2a7e3fc8606dd21ec
imane <imanezenasnii@gmail.com>
2023-10-10 00:04:31 +0100

    Extend BaseGeometry with integer_validator method for attribute validation

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module provides a base class for geometric shapes and operations
+"""
+
+
+class BaseGeometry():
+    """
+    A base class for geometric shapes and operations
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """ Validate that a value is an integer greater than 0
+        Args:
+            name (str): The name of the variable being validated
+            value: The value to be validated
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
