41e208b97dba56ae1879b6c9a0396456f6310239
imane <imanezenasnii@gmail.com>
2023-10-10 02:41:25 +0100

    Implement add_attribute function to add attributes to objects if possible

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,6 +5,9 @@ This module defines a function and a class for adding attributes to objects
 
 
 def add_attribute(obj, attribute, value):
+    """
+    Adds a new attribute to the object if possible
+    """
     if hasattr(obj, '__dict__'):
         obj.__dict__[attribute] = value
     else:
