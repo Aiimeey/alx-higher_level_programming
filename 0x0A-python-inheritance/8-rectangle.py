e609efeeec121657f611a4f3f8f7aa208fb42338
imane <imanezenasnii@gmail.com>
2023-10-10 01:01:44 +0100

    Create a Rectangle class that inherits from BaseGeometry and represents a rectangle with width and height attributes

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,7 +6,10 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """Defines a Rectangle"""
+
     def __init__(self, width, height):
+        """ initialized atribute """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
