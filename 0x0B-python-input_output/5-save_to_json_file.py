149075113b10be4c9a351847e4abb8eda6737efc
imane <imanezenasnii@gmail.com>
2023-10-10 19:29:40 +0100

    Implement function to parse JSON string into object

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""JSON String deserializer Module"""
+""" The function serializes the 'my_obj' parameter into a JSON format
+    and writes it to the specified file"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
+    """Serialize and save the given object to a JSON file"""
     with open(filename, "w", encoding="utf-8") as file:
         return json.dump(my_obj, file)
