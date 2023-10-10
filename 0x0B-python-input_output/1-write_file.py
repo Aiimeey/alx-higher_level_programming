8700e21f96742f440802a181cc718bef38b8d259
imane <imanezenasnii@gmail.com>
2023-10-10 16:49:44 +0100

    Add function to read and print file content

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """prints into a file and returns the number of char written"""
+    """write into a file and returns the number of char written"""
     with open(filename, "w", encoding="utf-8") as file:
         char_num = file.write(text)
         return char_num
