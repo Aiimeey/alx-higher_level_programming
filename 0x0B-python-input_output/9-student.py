53a53e8eef2e06feee88e793a4792541702d2805
imane <imanezenasnii@gmail.com>
2023-10-11 16:49:25 +0100

    Create Student class for JSON conversion

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" """
+""" script defines a Python class that represents a student's information """
 
 
 class Student():
-    """ """
+    """Represents a student's information."""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """ """
+        """Converts student attributes to a JSON-like dictionary """
         result = {}
         for key, value in self.__dict__.items():
             if isinstance(value, (list, dict, str, int, bool)):
