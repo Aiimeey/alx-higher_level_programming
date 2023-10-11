7e0d925428df2f5a3feabda92a08895096fcd066
imane <imanezenasnii@gmail.com>
2023-10-11 10:34:23 +0100

    Implement method to convert class instance to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+"""defines a function that converts an object's attributes into a dictionary"""
+
+
 def class_to_json(obj):
+    """Convert object attributes to a filtered dictionary """
     result = {}
     for key, value in obj.__dict__.items():
         if isinstance(value, (list, dict, str, int, bool)):
