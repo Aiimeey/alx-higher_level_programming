8292c4022df01a10c4a94959d4f969e3160b6990
imane <imanezenasnii@gmail.com>
2023-10-16 19:48:19 +0100

    Implement Base class methods for managing unique identifiers and JSON data

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -42,7 +42,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """ Converts a JSON string to a list of dictionaries """
-        if json_string is None or len(json_string) == 0 or json_string == "":
+        if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
 
