a48cc7c4fcec9e7b56a7c70c1db8324314a1d61b
imane <imanezenasnii@gmail.com>
2023-10-16 19:21:42 +0100

    Implement the Square class as a subclass of Rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-""" """
+"""Square module"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ """
+    """ This class represents a square, a special case of a rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """ Returns a string representation of the square."""
         return "[Square] ({}) {}/{} - {}"\
             .format(self.id, self.x, self.y, self.size)
 
