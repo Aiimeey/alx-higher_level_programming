ac62b47f497f08e6fd0b619f90e7ce0338babc86
imane <imanezenasnii@gmail.com>
2023-12-17 14:38:11 +0100

    Script for listing states starting with the letter 'N'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that lists all states starting with 'N' from the database hbtn_0e_0_usa 
+Script that lists all states starting with 'N' from the database hbtn_0e_0_usa
 """
 import MySQLdb
 from sys import argv
@@ -13,7 +13,8 @@ if __name__ == "__main__":
                         )
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY \
+    id ASC")
     data = cursor.fetchall()
 
     for row in data:
