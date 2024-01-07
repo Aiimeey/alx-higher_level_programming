bc6e66455537c0e9cd612e36d6de2660386e5f39
imane <imanezenasnii@gmail.com>
2024-01-08 00:53:19 +0100

    python-network_1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status
+"""
+from urllib import request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with request.urlopen(url) as response:
+        content = response.read()
+
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", content.decode('utf-8'))
