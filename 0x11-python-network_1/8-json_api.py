2a31d56748d4dfa51b924a6f21046d0e5db97dc2
imane <imanezenasnii@gmail.com>
2024-01-08 01:43:26 +0100

    python-network_1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-Takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter. Displays the id and name if the response body
+Takes in a data and sends a POST request to http://0.0.0.0:5000/search_user
+with the data as a parameter. Displays the id and name if the response body
 is properly JSON formatted and not empty, otherwise displays appropriate messages.
 """
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    payload = {'q': letter}
+    data = argv[1] if len(argv) > 1 else ""
     
-    response = requests.post(url, data=payload)
+    response = requests.post(url, data={'q': data})
     try:
         data = response.json()
         if data:
