45296df35d4052f664719d5ff53bbbd59f41e71e
imane <imanezenasnii@gmail.com>
2024-01-08 01:03:39 +0100

    python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,12 @@ Fetches https://alx-intranet.hbtn.io/status
 """
 from urllib import request
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    with request.urlopen(url) as response:
-        content = response.read()
 
-    print("Body response:")
-    print('\t- type: {}'.format(type(content)))
-    print('\t- content: {}'.format(content))
-    print('\t- utf8 content: {}'.format(content.decode('utf-8')))
+if __name__ == "__main__":
+
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(content)))
+        print('\t- content: {}'.format(content))
+        print('\t- utf8 content: {}'.format(content.decode('utf-8')))
