bf5b411f6fe27b13e44ee237d031dc9b40d3c6f6
imane <imanezenasnii@gmail.com>
2024-01-10 02:06:01 +0100

    Add python network 0 project

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-# A header variable X-School-User-Id must be sent with the value 98
-curl -sH "X-School-User-Id: 98" "$1"
+# Script that takes in a URL as an argument,sends GET request,displays response
+curl -sX GET -H "X-School-User-Id: 98" "$1"
