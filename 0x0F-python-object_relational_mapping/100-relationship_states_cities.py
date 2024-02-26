a99e54f46b7d130be05146883695cb640873c0a6
imane <imanezenasnii@gmail.com>
2024-02-26 23:34:58 +0100

    Add advanced task

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,15 +10,15 @@ if __name__ == '__main__':
         print("Usage: {} username password database_name".format(argv[0]))
         exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
     session = Session(engine)
 
-    california = State(name='Californiaa')
+    california = State(name='California')
 
-    san_francisco = City(name='San Franciscoo', state=california)
+    san_francisco = City(name='San Francisco', state=california)
 
     session.add(california)
     session.add(san_francisco)
