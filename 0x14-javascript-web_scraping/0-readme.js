998cf8de5f66b8e4fab24df247e598beaea2d775
imane <imanezenasnii@gmail.com>
2024-02-27 16:07:25 +0100

    Add a script that read a prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const fs = require('fs');
+/*
+console.log("Executable path:", process.argv[0]);
+console.log("Script path:", process.argv[1]);
+console.log("Command-line arguments:", process.argv[2]);
+*/
+if (process.argv.length > 2) {
+    const filePath = process.argv[2];
+
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(data);
+    });
+    
+}
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,4 @@
+[![js-semistandard-style](https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg)](https://github.com/standard/semistandard)
+Project Description: JavaScript - Web Scraping
+
+This project focuses on utilizing JavaScript for web scraping tasks. The tasks involve reading and writing files, making HTTP GET requests, and interacting with the Star Wars API to retrieve information about movies and characters.
