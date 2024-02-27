a8a690331a1a3a944bd109a696182551688cc8d2
imane <imanezenasnii@gmail.com>
2024-02-27 02:03:25 +0100

    Add Advanced task

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" relationship_state_client module """
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, Session
+from sys import argv
+
+Base = declarative_base()
+
+
+class State(Base):
+    """ class state """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state",
+                          cascade="all, delete")
+
+
+class City(Base):
+    """ city class """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State", back_populates="cities")
+
+
+if __name__ == "__main__":
+
+    if len(argv) != 4:
+        print("Usage: {} username password database_name".format(argv[0]))
+        exit(1)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"    {city.id}: {city.name}")
+
+    session.close()
