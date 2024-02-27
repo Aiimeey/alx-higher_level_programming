8bfeed61d3a1f04c4badd9cbbab2512cfd39671f
imane <imanezenasnii@gmail.com>
2024-02-27 23:47:54 +0100

    Add javascript web scraping project

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,28 +6,27 @@ const request = require('request');
 const [, , url, filePath] = process.argv;
 
 // Function to fetch webpage content and store it in a file
-function fetchAndStore(url, filePath) {
-    // Make request to the URL
-    request(url, (error, response, body) => {
-        if (error) {
-            console.error('Error occurred while fetching the webpage:', error);
-            return;
-        }
+function fetchAndStore (url, filePath) {
+  // Make request to the URL
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error('Error occurred while fetching the webpage:', error);
+      return;
+    }
 
-        if (response.statusCode !== 200) {
-            console.error('Failed to fetch webpage. Status code:', response.statusCode);
-            return;
-        }
+    if (response.statusCode !== 200) {
+      console.error('Failed to fetch webpage. Status code:', response.statusCode);
+      return;
+    }
 
-        // Write body content to file
-        fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
-            if (err) {
-                console.error('Error occurred while writing to file:', err);
-                return;
-            }
-            //console.log('Webpage content successfully stored in', filePath);
-        });
+    // Write body content to file
+    fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
+      if (err) {
+        console.error('Error occurred while writing to file:', err);
+      }
+      // console.log('Webpage content successfully stored in', filePath);
     });
+  });
 }
 
 // Call function to fetch and store webpage content
