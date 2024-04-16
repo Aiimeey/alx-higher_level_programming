af43f6153b977abcad009c6278e2b0fe616d6ee4
Aiimey <132620366+Aiimeey@users.noreply.github.com>
2024-04-16 20:16:55 +0100

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,7 @@ const url = process.argv[2];
 /*
 for (let i = 0; i < process.argv.length; i++) {
   console.log(`Argument ${i}: ${process.argv[i]}`);
+  URL to request : ./2-statuscode.js https://alx-intranet.hbtn.io/status
 }
 */
 request.get(url, (error, response) => {
