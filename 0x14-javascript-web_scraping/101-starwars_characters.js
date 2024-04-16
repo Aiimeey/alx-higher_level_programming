8211cbe8ec11fd1cbd631eeded939803cb116f56
Aiimey <132620366+Aiimeey@users.noreply.github.com>
2024-04-17 00:57:19 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+function check (error, response) {
+  if (error) {
+    console.error(error);
+  }
+}
+request(url, async (error, response, body) => {
+  check(error, response);
+
+  JSON.parse(body).characters.forEach(x => {
+    request(x, async (error, response, body) => {
+      check(error, response);
+      x = JSON.parse(body);
+      console.log(x.name);
+    });
+  });
+});
+
